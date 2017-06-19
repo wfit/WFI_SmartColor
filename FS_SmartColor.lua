17e7570349566c2c5a8ef1157df683a1bbb49da0
Bastien Clément <g@ledric.me>
2017-06-19 19:54:08 +0200

    Make it unrelated to Grid

---
+++ FS_SmartColor.lua
@@ -0,0 +1,73 @@
+local _, Addon = ...
+SmartColor = LibStub("AceAddon-3.0"):NewAddon(Addon, "FS_SmartColor", "AceEvent-3.0")
+
+local filters = {}
+local currentKeys = {}
+local modules = {}
+
+function SmartColor:OnInitialize()
+	self:RegisterMessage("FS_MSG_SMARTCOLOR")
+end
+
+function SmartColor:RegisterFilter(filter)
+	filters[filter] = true
+end
+
+function SmartColor:UnregisterFilter(filter)
+	filters[filter] = nil
+end
+
+function SmartColor:RegisterModule(module)
+	modules[#modules + 1] = module
+end
+
+function SmartColor:FS_MSG_SMARTCOLOR(_, msg)
+	local action = msg.action
+	local key = msg.key
+	if action == "set" then
+		if key ~= nil then
+			for filter in pairs(filters) do
+				if filter:SmartColorFilter(key) == false then
+					return
+				end
+			end
+		end
+		self:Set(key, msg.guid, msg.color)
+	elseif action == "unset" then
+		self:Unset(key, msg.guid)
+	elseif action == "unsetall" then
+		self:UnsetAll(key)
+	end
+end
+
+function SmartColor:Set(key, guid, color)
+	currentKeys[guid] = key
+	for _, module in ipairs(modules) do
+		module:Set(guid, color)
+	end
+end
+
+function SmartColor:Unset(key, guid)
+	if currentKeys[guid] == nil or currentKeys[guid] == key then
+		currentKeys[guid] = nil
+		for _, module in ipairs(modules) do module:Unset(guid) end
+	end
+end
+
+function SmartColor:UnsetAll(key)
+	if key ~= nil then
+		local removed = {}
+		for guid, k in pairs(currentKeys) do
+			if k == key then
+				for _, module in ipairs(modules) do module:Unset(guid) end
+				removed[#removed + 1] = guid
+			end
+		end
+		for _, guid in ipairs(removed) do
+			currentKeys[guid] = nil
+		end
+	else
+		for _, module in ipairs(modules) do module:UnsetAll() end
+		wipe(currentKeys)
+	end
+end
---
+++ Grid.lua
@@ -0,0 +1,42 @@
+local _, SmartColor = ...
+if Grid then
+	local GridStatusSmartColor = Grid:GetModule("GridStatus"):NewModule("GridStatusSmartColor")
+
+	GridStatusSmartColor.defaultDB = {
+		smart_color = {
+			text = "SmartColor™",
+			enable = true,
+			color = { r = 1.0, g = 1.0, b = 1.0, a = 1.0 },
+			priority = 90,
+		}
+	}
+
+	function GridStatusSmartColor:PostInitialize()
+		self:RegisterStatus("smart_color", "SmartColor™", nil, true)
+	end
+
+	function GridStatusSmartColor:OnStatusEnable(status)
+		if status == "smart_color" then
+		end
+	end
+
+	function GridStatusSmartColor:OnStatusDisable(status)
+		if status == "smart_color" then
+			self:UnsetAll()
+		end
+	end
+
+	function GridStatusSmartColor:Set(guid, color)
+		self.core:SendStatusGained(guid, "smart_color", self.db.profile.smart_color.priority, nil, color)
+	end
+
+	function GridStatusSmartColor:Unset(guid)
+		self.core:SendStatusLost(guid, "smart_color")
+	end
+
+	function GridStatusSmartColor:UnsetAll()
+		self.core:SendStatusLostAllUnits("smart_color")
+	end
+
+	SmartColor:RegisterModule(GridStatusSmartColor)
+end
